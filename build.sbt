569a83525f47f2b179eb2d33ca55896c311cda92
LudmilaNovikova <Ludmila.Novikova@artezio.com>
2016-01-31 23:00:51 +0300

    org.apache.spark.SparkException: Couldn't find leader offsets for Set([SbtStream,0])

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "CableTv"
+
+version := "1.0"
+
+scalaVersion := "2.10.5"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" % "spark-core_2.10" % "1.6.0" % "provided",
+  "org.apache.spark" % "spark-streaming_2.10" % "1.6.0" % "provided",
+  "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.6.0" % "provided",
+  "javax.servlet" % "javax.servlet-api" % "3.0.1" % "compile"
+)
---
+++ src/main/scala/big/data/cable/tv/KafkaStreamProcessing.scala
@@ -0,0 +1,59 @@
+package big.data.cable.tv
+
+import kafka.serializer.StringDecoder
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka._
+/**
+ * Created by lnovikova on 30.01.2016.
+ */
+object KafkaStreamProcessing {
+
+  def main(args: Array[String]): Unit = {
+
+/*
+    if (args.length < 1) {
+      System.err.println(s"""
+        |Usage: KafkaStreamProcessing <brokers>
+        |  <brokers> is a list of one or more Kafka brokers
+        |
+        """.stripMargin)
+      System.exit(1)
+    }
+*/
+//    val brokers = "sandbox.hortonworks.com:6667"//args(0)
+//    val brokers = "192.168.253.129:2181"//args(0)
+    val brokers = "192.168.253.129:6667"//args(0)
+
+    val sparkConf = new SparkConf()
+    sparkConf.setAppName("KafkaStreamProcessing")
+    sparkConf.setMaster("local[2]")
+    val sc = new SparkContext(sparkConf)
+
+    val ssc = new StreamingContext(sc, Seconds(2))
+
+    // Create direct kafka stream with brokers and topics
+    val topic = "SbtStream"
+    val topicsSet = Set(topic)
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+    val messages = KafkaUtils.createDirectStream(ssc, kafkaParams, topicsSet)
+
+    println(messages)
+    messages.saveAsTextFiles("cableTvData", "txt")
+
+    // Start the computation
+    ssc.start()
+    ssc.awaitTermination()
+
+    sc.stop();
+
+/*
+
+    val directKafkaStream = KafkaUtils.createDirectStream[
+      [key class], [value class], [key decoder class], [value decoder class] ](
+      streamingContext, [map of Kafka parameters], [set of topics to consume])
+*/
+
+  }
+
+}
