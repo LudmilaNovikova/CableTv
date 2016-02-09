92f0ff291276e20a1fa11c24754cd2ab2ec3f087
LudmilaNovikova <Ludmila.Novikova@artezio.com>
2016-02-09 21:21:56 +0300

    + hive

--- build.sbt
+++ build.sbt
@@ -8,6 +8,8 @@ libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-core_2.10" % "1.5.0" % "provided",
   "org.apache.spark" % "spark-streaming_2.10" % "1.5.0" % "provided",
   "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.5.0" % "provided",
+  "org.apache.spark" % "spark-sql_2.10" % "1.5.0" % "provided",
+  "org.apache.spark" % "spark-hive_2.10" % "1.5.0" % "compile",
   "javax.servlet" % "javax.servlet-api" % "3.0.1" % "compile"
 )
 
--- src/main/scala/big/data/cable/tv/KafkaStreamProcessing.scala
+++ src/main/scala/big/data/cable/tv/KafkaStreamProcessing.scala
@@ -1,9 +1,12 @@
 package big.data.cable.tv
 
+import org.apache.spark.sql.{DataFrameWriter, SaveMode}
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka._
 import kafka.serializer.StringDecoder
+import org.apache.spark.sql.hive.HiveContext
+
 /**
  * Created by lnovikova on 30.01.2016.
  */
@@ -11,24 +14,28 @@ object KafkaStreamProcessing {
 
   def main(args: Array[String]): Unit = {
 
-/*
-    if (args.length < 1) {
-      System.err.println(s"""
-        |Usage: KafkaStreamProcessing <brokers>
-        |  <brokers> is a list of one or more Kafka brokers
-        |
-        """.stripMargin)
-      System.exit(1)
-    }
-*/
-    val brokers = "bigdata1.nnstu.com:9092"//args(0)
+    /*
+        if (args.length < 1) {
+          System.err.println(s"""
+            |Usage: KafkaStreamProcessing <brokers>
+            |  <brokers> is a list of one or more Kafka brokers
+            |
+            """.stripMargin)
+          System.exit(1)
+        }
+    */
+    val brokers = "bigdata1.nnstu.com:9092" //args(0)
 
     val sparkConf = new SparkConf()
     sparkConf.setAppName("KafkaStreamProcessing")
     sparkConf.setMaster("local[2]")
     val sc = new SparkContext(sparkConf)
-
     val ssc = new StreamingContext(sc, Seconds(2))
+    System.setProperty("hadoop.home.dir", "C:\\winutil\\")
+    val sqlContext = new HiveContext(sc)
+    import sqlContext.implicits._
+
+    createHiveTableSbtStructuredMessages(sqlContext)
 
     // Create direct kafka stream with brokers and topics
     val topic = "SbtStream"
@@ -38,26 +45,85 @@ object KafkaStreamProcessing {
       "group.id" -> "sbtGroupId",
       "metadata.broker.list" -> brokers
     )
+
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-    messages.foreachRDD( rdd =>
-      { if(rdd.count() > 0){
-          println("Going to save to file: " + rdd.collect().foreach(println(_)))
-          rdd.saveAsTextFile("cableTvDataRdd2")
-          rdd.foreach ( record =>
-            println(record) // executed at the worker
-          )
-        }
+    messages.foreachRDD(rdd => {
+      if (rdd.count() > 0) {
+        println("Going to save to file: " + rdd.collect().foreach(println(_)))
+        // save to hdfs
+//        rdd.saveAsTextFile("cableTvDataRdd")
+        // save to Hive
+//        rdd.toDF().insertInto("SbtStructuredMessages")
+        rdd.toDF().write.mode(SaveMode.Append).insertInto("SbtStructuredMessages")
+//        new DataFrameWriter(rdd.toDF()).mode(SaveMode.Append).insertInto("SbtStructuredMessages")
+        rdd.foreach(record =>
+          println(record) // executed at the worker
+        )
       }
+    }
     )
     messages.print(5)
-//    messages.saveAsTextFiles("cableTvData", "txt")
-//    messages.saveAsHadoopFiles("cableTvData", "txt")
+    //    messages.saveAsTextFiles("cableTvData", "txt")
+    //    messages.saveAsHadoopFiles("cableTvData", "txt")
 
     // Start the computation
     ssc.start()
     ssc.awaitTermination()
 
     sc.stop();
+
+  }
+
+  def createHiveTableSbtStructuredMessages(sqlContext: HiveContext): Unit = {
+    println("Creating Hive table SbtStructuredMessages")
+    sqlContext.sql("CREATE TABLE IF NOT EXISTS SbtStructuredMessages (" +
+      "msgType String, " +
+      "streamType String, " +
+      "mesDate Timestamp, " +
+      "mesInterval Timestamp, " +
+      "mac String, " +
+      "streamAddr String, " +
+      "received Int, " +
+      "linkFaults Int, " +
+      "lostOverflow String, " +
+      "lost Int, " +
+      "restored Int, " +
+      "overflow Int, " +
+      "underflow Int, " +
+      "mdiDf Int, " +
+      "mdiMlr Double, " +
+      "plc String," +
+      "regionId Int," +
+      "serviceAccountNumber String," +
+      "stbIp String," +
+      "serverDate Timestamp, " +
+      "spyVersion String," +
+      "playerUrl String," +
+      "contentType Int," +
+      "transportOuter Int," +
+      "transportInner Int," +
+      "channelId Int," +
+      "playSession Int," +
+      "scrambled Int," +
+      "powerState Int," +
+      "uptime Int," +
+      "casType Int," +
+      "casKeyTime Int," +
+      "vidFrames Int," +
+      "vidDecodeErrors Int," +
+      "vidDataErrors Int," +
+      "audFrames Int," +
+      "audDataErrors Int," +
+      "avTimeSkew Int," +
+      "avPeriodSkew Int," +
+      "bufUnderruns Int," +
+      "bufOverruns Int," +
+      "sdpObjectId Int," +
+      "dvbLevelGood Int," +
+      "dvbLevel Int," +
+      "dvbFrequency Int," +
+      "curBitrate Int " +
+      ")")
 
   }
 
