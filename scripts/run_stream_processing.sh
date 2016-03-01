16229bc5150d98180ddd24922d3530b9cd82d934
LudmilaNovikova <Ludmila.Novikova@artezio.com>
2016-03-01 12:06:37 +0300

    comment println messages

--- scripts/run_stream_processing.sh
+++ scripts/run_stream_processing.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 sudo -u hdfs spark-submit --class big.data.cable.tv.KafkaStreamProcessing --master yarn-client --executor-memory 2G --num-executors 3 /opt/lnovikova/cabelTV/streaming/cabletv_2.10-1.0.jar #> /opt/lnovikova/cabelTV/streaming/log/cableTvStreaming.log
+ sudo -u hdfs spark-submit --class big.data.cable.tv.KafkaStreamProcessing --master yarn-client --executor-memory 2G --num-executors 3 /opt/lnovikova/cabelTV/streaming/cabletv_2.10-1.0.jar
+spark-submit --class big.data.cable.tv.KafkaStreamProcessing --master local[4] --executor-memory 2G --num-executors 3 /opt/lnovikova/cabelTV/streaming/cabletv_2.10-1.0.jar | tee /home/lnovikova/projects/cableTv/log/cableTvStreaming.log
