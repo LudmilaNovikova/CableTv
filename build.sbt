f7f4de5565290067839a63a69d49a0bca84cc903
lnovikova <ludmila.novikova@artezio.com>
2016-03-11 22:52:31 +0300

    add publish message to kafka topic SbtFailure

--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.5.0" % "provided",
   "org.apache.spark" % "spark-sql_2.10" % "1.5.0" % "provided",
   "org.apache.spark" % "spark-hive_2.10" % "1.5.0" % "compile",
+  "org.apache.kafka" % "kafka-clients" % "0.8.2.0",
   "javax.servlet" % "javax.servlet-api" % "3.0.1" % "compile"
 )
 
--- src/main/scala/big/data/cable/tv/KafkaStreamProcessing.scala
+++ src/main/scala/big/data/cable/tv/KafkaStreamProcessing.scala
@@ -1,6 +1,9 @@
 package big.data.cable.tv
 
+import java.util.Properties
+
 import big.data.cable.tv.service.{SbtStructuredMessageService, HiveService}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -29,10 +32,24 @@ object KafkaStreamProcessing {
 
     val sparkConf = new SparkConf()
     sparkConf.setAppName("KafkaStreamProcessing")
-//    sparkConf.setMaster("local[2]")
     val sc = new SparkContext(sparkConf)
     val ssc = new StreamingContext(sc, Seconds(2))
     val sqlContext = new HiveContext(sc)
+
+val props: Properties = new Properties
+    props.put("bootstrap.servers", "192.168.1.31:9092")
+//    props.put("zk.connect", args(1))
+    props.put("acks", "0")
+    props.put("retries", "0")
+    props.put("batch.size", "16384")
+    props.put("linger.ms", "1")
+    props.put("buffer.memory", "33554432")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val TOPIC: String = "SbtFailure"
+    val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](props)
+    producer.send(new ProducerRecord[String, String](TOPIC, "test again"))
 
     HiveService.createTableSbtStructuredMessage(sqlContext)
 
@@ -68,7 +85,6 @@ object KafkaStreamProcessing {
     // Start the computation
     ssc.start()
     ssc.awaitTermination()
-
     sc.stop();
 
   }
